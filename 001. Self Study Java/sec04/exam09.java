94943f5901a29bc7d36e2859927ff3552a9f63b5
정재오 <chaa822@naver.com>
2022-07-13 10:29:20 +0900

    sec04

---
+++ 001. Self Study Java/sec04/exam09.java
@@ -0,0 +1,11 @@
+package sec04;
+
+public class exam09 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 10; i ++){
+			System.out.println(i);
+		}
+	}
+}
---
+++ 001. Self Study Java/sec04/exam14.java
@@ -0,0 +1,12 @@
+package sec04;
+
+public class exam14 {
+	
+	public static void main(String[] args) {
+		int i = 1;
+		while ( i <= 10 ){
+			System.out.println(i);
+			i++;
+		}
+	}
+}
